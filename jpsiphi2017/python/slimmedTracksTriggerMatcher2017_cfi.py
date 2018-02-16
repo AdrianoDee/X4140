fa86a2919c1a1fe569984581b39626874b5260cc
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-16 19:38:47 +0100

    unpacked

--- jpsiphi2017/python/slimmedTracksTriggerMatcher2017_cfi.py
+++ jpsiphi2017/python/slimmedTracksTriggerMatcher2017_cfi.py
@@ -2,13 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 ### ==== This is our version of the patPFCandsWithTrigger using MINIAOD
 
-### unpack them
-unpackedPatTrigger = cms.EDProducer("PATTriggerObjectStandAloneUnpacker",
-  patTriggerObjectsStandAlone = cms.InputTag( 'slimmedPatTrigger' ),
-  triggerResults              = cms.InputTag( 'TriggerResults::HLT' ),
-  unpackFilterLabels          = cms.bool( True )
-)
-
 ### then perform a match for all HLT triggers of interest
 PATPFCandsTriggerMatchHLT = cms.EDProducer( "PATTriggerMatcherDRDPtLessByR",
     src     = cms.InputTag( "packedPFCandidates" ),
